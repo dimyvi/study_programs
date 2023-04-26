7ed0fb4f3f06e628842b216e8eb81d9c5d153596
Dima <dima@vib.com>
2023-04-26 16:57:06 +0300

    repetition

---
+++ course 2/module 2. repetition/body_mass_index.py
@@ -0,0 +1,10 @@
+mass = float(input())
+height = float(input())
+
+body_mass_index = mass/height**2
+if 18.5 <= body_mass_index <= 25:
+    print('Оптимальная масса')
+elif body_mass_index < 18.5:
+    print('Недостаточная масса')
+else:
+    print('Избыточная масса')
---
+++ course 2/module 2. repetition/line_cost.py
@@ -0,0 +1,3 @@
+from math import ceil
+count = len(input())
+print(int((count * 0.6) // 1), 'р.', ceil(float((count * 0.6) % 1)*100), 'коп.')

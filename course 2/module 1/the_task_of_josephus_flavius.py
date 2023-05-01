cd0a84e137e518ba31a947c7ecddd230dd8a6b00
Dima <dima@vib.com>
2023-05-01 16:21:04 +0300

    iosephus flavius

--- course 2/module 1/the_task_of_josephus_flavius.py
+++ course 2/module 1/the_task_of_josephus_flavius.py
@@ -1,11 +1,9 @@
 number_of_people = int(input())
 shift = int(input())
 
-list_of_people = [number for number in range(1, number_of_people+1)]
-print(list_of_people)
-
+list_of_people = [number for number in range(1, number_of_people + 1)]
 while len(list_of_people) > 1:
-    for i in list_of_people:
-        print(list_of_people[shift-1])
-        del list_of_people[shift-1]
-        print(list_of_people)
+    for i in range(0, shift - 1):
+        list_of_people.append(list_of_people[i])
+    del list_of_people[0:shift]
+print(*list_of_people)

42ba93fe359612304daba2edf9adc1c94f5a49a6
Dima <dima@vib.com>
2023-05-10 20:49:46 +0300

    new

--- course 2/module 4/packing_duplicates.py
+++ course 2/module 4/packing_duplicates.py
@@ -1,9 +1,9 @@
-ls = [i for i in input().split(' ')]
-ls_output = []
-for idx in range(len(ls) + 1):
-   new_ls = []
-   if ls[idx] == ls[idx + 1]:
-       new_ls.append(ls[idx])
-   print(new_ls)
+ls = []
 
+for i in input().split():
+    if ls and i in ls[-1]:
+        ls[-1].append(i)
+    else:
+        ls.append([i])
 
+print(ls)

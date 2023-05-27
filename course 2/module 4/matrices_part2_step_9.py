6356616cf538add05a81e4e046edf2162d32eb7b
Dima <dima@vib.com>
2023-05-27 22:02:36 +0300

    Matrix

--- course 2/module 4/matrices_part2_step_9.py
+++ course 2/module 4/matrices_part2_step_9.py
@@ -1,23 +1,38 @@
-n = int(input())
-mx = [[int(i) for i in input().split()] for _ in range(n)]
+def sum_over_columns_and_rows(ls):
+    sum_i, sum_j = 0, 0
+    full_i, full_j = 0, 0
+    full_lis = []
 
-s_i = 0
-full_i = 0
-s_j = 0
-full_j = 0
-for i in range(len(mx)):
-    for j in range(len(mx)):
-        s_i += int(str(mx[i][j]))
-        full_i += int(str(mx[i][j]))
-        s_j += int(str(mx[j][i]))
-        full_j += int(str(mx[j][i]))
-    print(s_j)
-    s_j = 0
-    print(s_i)
-    s_i = 0
+    for i in range(len(ls)):
+        for j in range(len(ls)):
+            sum_i += int(str(ls[i][j]))
+            full_i += int(str(ls[i][j]))
+
+            sum_j += int(str(ls[j][i]))
+            full_j += int(str(ls[j][i]))
+
+        full_lis.append(sum_j)
+        full_lis.append(sum_i)
+        sum_j, sum_i = 0, 0
+
+    return len(set(full_lis)) == 1 and full_j == full_i
 
 
-print()
+def full_numbers(ls):
+    static_list = [int(i) for i in range(1, len(ls) ** 2 + 1)]
+    ls_n = []
+    for i in range(len(ls)):
+        for j in range(len(ls)):
+            ls_n.append(ls[i][j])
+    ls_n.sort()
+    return ls_n == static_list
 
-print(full_i)
-print(full_j)
+
+mx = [[int(i) for i in input().split()] for _ in range(int(input()))]
+first_function = (sum_over_columns_and_rows(mx))
+second_function = (full_numbers(mx))
+
+if first_function and second_function:
+    print('YES')
+else:
+    print('NO')

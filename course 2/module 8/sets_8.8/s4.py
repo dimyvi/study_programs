19b75cc627ca9a0adce03ccc833251707b3a7481
Dima <dima@vib.com>
2023-06-05 17:19:29 +0300

    Sets python

---
+++ course 2/module 8/sets_8.8/s4.py
@@ -0,0 +1,4 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon',
+         'tangerine', 'Watermelon', 'currant', 'Almond']
+s = set(i.lower()[0] for i in words)
+print(*sorted(s))

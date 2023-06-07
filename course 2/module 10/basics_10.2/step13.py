60b382dbddac6f66efe6a12d7657b406921c53bf
Dima <dima@vib.com>
2023-06-07 15:09:26 +0300

    Dictionary

---
+++ course 2/module 10/basics_10.2/step13.py
@@ -0,0 +1,17 @@
+d = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+}
+ls = []
+for i in input():
+    if i.isdigit():
+        ls.append(d[int(i)])
+print(*ls)

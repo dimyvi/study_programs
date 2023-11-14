25a656e88415ea1810739979fe6910c9cf3660e3
dimyvi <dmitryvybornov@gmail.com>
2023-11-14 21:53:17 +0300

    step

--- course2/module 12/part_2/step_9.py
+++ course2/module 12/part_2/step_9.py
@@ -1,28 +1,24 @@
 import string
-from random import *
+from random import choice, shuffle
+
+chars1 = [с for с in string.ascii_uppercase if с not in 'OI']
+chars2 = [с for с in string.ascii_lowercase if с not in 'ol']
+chars3 = list(string.digits[2:])
+chars = chars1 + chars2 + chars3
 
 
 def generate_password(length):
-    a = string.digits + string.ascii_lowercase + string.ascii_uppercase
-    alf = [i for i in a if i not in '01LIOlio']
-    password = ''
-    while len(password) != length:
-        for k in sample(alf, length):
-            if k in string.digits:
-                password += k
-            if k in string.ascii_lowercase:
-                password += k
-            if k in string.ascii_uppercase:
-                password += k
-
+    result = [choice(i) for i in (chars1, chars2, chars3)] + [choice(chars) for _ in range(3, length)]
+    shuffle(result)
+    return ''.join(result)
 
 
 def generate_passwords(count, length):
-    ls = []
-    while len(ls) != count:
-        ls.append(generate_password(length))
-    print(*ls, sep='\n')
+    result = set()
+    while len(result) < count:
+        result.add(generate_password(length))
+    return list(result)
 
 
-n, m = int(input()), int(input())
-generate_passwords(n, m)
+for i in generate_passwords(int(input()), int(input())):
+    print(i)

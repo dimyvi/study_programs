b00ed34919ce066a892157077d62ace63ef0c6cb
dimyvi <dmitryvybornov@gmail.com>
2023-11-16 00:13:19 +0300

    method2

---
+++ course2/module 12/monte_carlo_method/step_2.py
@@ -0,0 +1,16 @@
+import random
+
+n = 10**6
+s0 = 4
+k = 0
+for _ in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
+    if x**2 + y**2 <= 1:
+        k += 1
+
+print((k/n)*s0)
+
+
+

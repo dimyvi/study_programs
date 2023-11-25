11f3af12748f919f964c54ed4646067291205e0b
dimyvi <dmitryvybornov@gmail.com>
2023-11-26 01:54:07 +0300

    function

--- course2/module 15/functions_with_variable_number_of_arguments_15_2/step_4.py
+++ course2/module 15/functions_with_variable_number_of_arguments_15_2/step_4.py
@@ -1,2 +1,9 @@
+def greet(*args):
+    args[0]
+    ls = [i for i in args]
+    s = ' and '.join(j for j in ls)
+    print('Hello, ' + s + '!', sep='')
 
-
+greet('Timur')
+greet('Timur', 'Roman')
+greet('Timur', 'Roman', 'Ruslan')

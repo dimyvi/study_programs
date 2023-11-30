c5d95504fbfda1b80d0b5c8ec6d46e6beb677e1b
dimyvi <dmitryvybornov@gmail.com>
2023-11-30 23:36:08 +0300

    function

--- course2/module 15/functions_as_objects_15_4/step_4.py
+++ course2/module 15/functions_as_objects_15_4/step_4.py
@@ -1,9 +1,12 @@
 athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30),
             ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
 
-
+k = int(input()) - 1
 def func(a):
-    return max(a)
+    return a[k]
 
-athletes.sort(athletes, key=func)
-print(athletes)
+
+athletes.sort(key=func)
+
+for i in athletes:
+    print(*i)

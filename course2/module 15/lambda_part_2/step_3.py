435b83e30fe712f70b1d9a394bc1653696fc354e
dimyvi <dmitryvybornov@gmail.com>
2023-12-15 01:05:34 +0300

    lambda

---
+++ course2/module 15/lambda_part_2/step_3.py
@@ -0,0 +1,9 @@
+is_non_negative_num = lambda x: True if str(x[0]) != '-' and  else False
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))

4103d108a019a5f6c17104339a8e08c57eb8351e
dimyvi <dimyvybornov@gmail.com>
2024-06-13 16:30:45 +0300

    update

---
+++ course3/module 4/4_3/step_1.py
@@ -0,0 +1,9 @@
+import json
+
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+countries_json = json.dumps(countries, indent=3, sort_keys=True, separators=(',', ' - '))
+print(countries_json)

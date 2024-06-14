1d98469f682fbd951d65167b62fe88d56d501aab
dimyvi <dimyvybornov@gmail.com>
2024-06-14 18:29:43 +0300

    update

---
+++ course3/module 4/4_3/step_7.py
@@ -0,0 +1,24 @@
+import json
+
+with open('data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    new_file = []
+    for position in data:
+        if isinstance(position, str):
+            new_file.append(position + '!')
+        elif isinstance(position, int) and not isinstance(position, bool):
+            new_file.append(position + 1)
+        elif isinstance(position, bool):
+
+            new_file.append(not position)
+        elif isinstance(position, list):
+            position.extend(position)
+            new_file.append(position)
+        elif isinstance(position, dict):
+            position['newkey'] = None
+            new_file.append(position)
+        else:
+            continue
+
+with open('updated_data.json', 'w', encoding='utf-8') as out_file:
+    json.dump(new_file, out_file, indent=3)

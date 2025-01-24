87e81ac9beeac742d6925f6be13bf678bc985ad5
dimyvi <dimyvybornov@gmail.com>
2025-01-24 14:13:46 +0300

    new

--- course3/module 9/step_26.py
+++ course3/module 9/step_26.py
@@ -5,12 +5,15 @@ def square(func):
     @wraps(func)
     def wrappers(*args, **kwargs):
         ans = func(*args, **kwargs)
-        return ans**2
+        return ans ** 2
+
     return wrappers
+
 
 @square
 def add(a, b):
     return a + b
+
 
 print(add(3, 7))
 print(add.__name__)

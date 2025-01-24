bbe1e527e15d4e1b62160085aad734a8d44ab512
dimyvi <dimyvybornov@gmail.com>
2025-01-24 14:12:03 +0300

    new

---
+++ course3/module 9/step_26.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+
+def square(func):
+    @wraps(func)
+    def wrappers(*args, **kwargs):
+        ans = func(*args, **kwargs)
+        return ans**2
+    return wrappers
+
+@square
+def add(a, b):
+    return a + b
+
+print(add(3, 7))
+print(add.__name__)
+print(add.__doc__)
